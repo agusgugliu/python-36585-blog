00434a79525c72e3e27b9a1bda026d3b4a95c5b1
Agustin Gugliuzza <agugliuzzapicci@gmail.com>
2022-06-15 14:05:50 -0300

    Created HTML templates for App_Blog

--- Blog_Final/App_Blog/urls.py
+++ Blog_Final/App_Blog/urls.py
@@ -3,11 +3,11 @@ from App_Blog import views
 
 urlpatterns = [
     path('', views.homepage, name='homepage'),
-    path('blog/', views.BlogList.as_view(), name='notas'),
-    path('new_blog/', views.BlogCreate.as_view(), name='blog_create'),
-    path('details/<pk>/', views.BlogDetail.as_view(), name ='blog_detail'),
-    path('edit/<pk>/', views.BlogUpdate.as_view(), name ='blog_update'),
-    path('delete/<pk>/', views.BlogDelete.as_view(), name ='blog_delete'),
+    path('blog/', views.BlogList.as_view(), name='notas_list'),
+    path('new_blog/', views.BlogCreate.as_view(), name='notas_create'),
+    path('details/<pk>/', views.BlogDetail.as_view(), name ='notas_detail'),
+    path('edit/<pk>/', views.BlogUpdate.as_view(), name ='notas_update'),
+    path('delete/<pk>/', views.BlogDelete.as_view(), name ='notas_delete'),
     path('login/', views.BlogLogin.as_view(), name='user_login'),
     path('logout/', views.BlogLogout.as_view(), name='user_logout'),
 ]
