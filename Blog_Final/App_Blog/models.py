7057b1b29436f6f7cd5614b31f9e3eddc40651dc
Agustin Gugliuzza <agugliuzzapicci@gmail.com>
2022-06-17 12:25:48 -0300

    Modified App_Blog.models to be able to UPDATE or DELETE blogs.

--- Blog_Final/App_Blog/models.py
+++ Blog_Final/App_Blog/models.py
@@ -1,12 +1,12 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 
 class ModelBlog(models.Model):
     titulo = models.CharField(max_length=100)
     subtitulo = models.CharField(max_length=100)
     cuerpo = models.TextField()
-    autor = models.CharField(max_length=100)
+    autor = models.ForeignKey(User, on_delete=models.CASCADE)
     fecha = models.DateField(auto_now_add=True)
 
     def __str__(self):
